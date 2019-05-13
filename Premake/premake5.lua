e5842baac833891d99b31671b5e1720d4ffd26e2
kyle <zh880517@163.com>
2019-05-13 23:00:39 +0800

    修改工程文件

--- Premake/premake5.lua
+++ Premake/premake5.lua
@@ -1,7 +1,20 @@
-project "rebuildproject"
-    language "C++"
-    kind "Utility"
-    prebuildcommands {"cd ../", "call make_vs_dev.bat"}
-    files{
-        "*.lua",
-    }
+workspace "AsioNet"
+    configurations { "Debug", "Release" }
+    location "../Build"
+    cppdialect "C++11"
+            
+    filter "configurations:Debug"
+        defines     "_DEBUG"
+        symbols     "On"
+    filter "configurations:Release"
+        defines     "NDEBUG"
+        optimize    "Full"
+    project "rebuildproject"
+        language "C++"
+        kind "Utility"
+        prebuildcommands {"cd ../", "call make_vs_dev.bat"}
+        files{
+            "*.lua",
+        }
+        
+    include "../Source/AsioKCP"
--- make_vs_dev.bat
+++ make_vs_dev.bat
@@ -1,4 +1,4 @@
 cd %~dp0
 cd ./Premake
-premake5.exe --file=premake_dev.lua vs2017
+premake5.exe vs2017
 cd %~dp0
